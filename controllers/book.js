fb69a0ce492b63d97253f1a542dc1e1e14335664
Atilia Thomas <atiliaa.thomas@gmail.com>
2024-12-07 23:47:43 -0500

    add book controller file

---
+++ controllers/book.js
@@ -0,0 +1,44 @@
+const Book = require('../models/bookModel'); 
+
+exports.getBooks = async (req, res, next) => { 
+    try { 
+        const books = await Book.find(); 
+        res.json(books);
+    } catch (error) { 
+        next(error);
+    }
+}; 
+
+exports.createBook = async (req, res, next) => { 
+    try { 
+        const book = new Book(req.body); 
+        await book.save(); 
+        res.status(201).json(book);
+    } catch (error) { 
+        next(error);
+    }
+} ; 
+
+exports.updateBook = async (req, res, next) => { 
+    try { 
+        const book = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true}); 
+        if (!book) { 
+            return res.status(404).json({error: 'Book not found'});
+        } 
+        res.json(book); 
+    } catch (error) { 
+        next(error);
+    }
+};
+
+exports.deleteBook = async (req, res, next) => { 
+    try { 
+        const book = await Book.findByIdAndDelete(req.params.id); 
+        if (!book) { 
+            return res.status(404).json({error: 'Book not found' }); 
+        } 
+        res.json({ message: 'Book deleted successfully'});
+        } catch (error) { 
+            next(error);
+        }
+    };
